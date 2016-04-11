aeed26e296c69acab21acee4a1755f4d4408b926
Rex Graff <rgraff3@gmail.com>
2016-04-11 13:18:41 -0400

    Completing assignment
    Descriptions updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions are objects which perform a specific action.  Kind of like a mini program within a program.
 
-## Write a short comment describing this function
+## As described in the assignment, "This function creates a special "matrix" object that can cache its inverse."
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## As described in the assignment, "This function computes the inverse of the special "matrix" returned by `makeCacheMatrix` above. If the inverse has already been calculated (and the matrix has not changed), then `cacheSolve` should retrieve the inverse from the cache."
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
